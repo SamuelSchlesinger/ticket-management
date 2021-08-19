2e08cfb7e68d0e49d9a4bd361fed511dd84272fc
Samuel Schlesinger <samuel@simspace.com>
2021-08-19 14:04:44 -0400

    Added better tests, refactored app a bit

--- src/TicketManager.hs
+++ src/TicketManager.hs
@@ -10,6 +10,92 @@ import Imports
 import qualified Data.Set as Set
 import qualified Data.ByteString as BS
 
+program :: TicketStatement -> IO ()
+program ticketStatement = lookupEnv "TICKET_SYSTEM" >>= \case
+  Nothing -> do
+    fail "No TICKET_SYSTEM environment variable set"
+  Just filepath -> do
+    case ticketStatement of
+      CommandStatement cmd -> executeCommands filepath [cmd]
+      QueryStatement q -> withTicketSystem filepath (mapM_ print . queryModel q . ticketModel)
+      InitializeStatement -> do
+        doesFileExist filepath >>= \case
+          True -> fail "Trying to initialize a pre-existing ticket system"
+          False -> BS.writeFile filepath (encode emptyTicketSystem)
+      ValidateStatement -> do
+        withTicketSystem filepath \ts -> do
+          case appendCommands (ticketCommands ts) emptyTicketSystem of
+            Nothing -> fail "Ticket system's commands are invalid"
+            Just ts' -> do
+              when (ts' /= ts) (fail "Ticket system's commands do not lead to the model present")
+              putStrLn "Ticket system is valid"
+
+parser :: ParserInfo TicketStatement
+parser = flip info mods . hsubparser . mconcat $
+  [ command "create" (info create (progDesc "Create a new ticket"))
+  , command "edit" (info edit (progDesc "Edit the name, description, and status of an existing ticket"))
+  , command "relate" (info relate (progDesc "Relate one ticket to another"))
+  , command "unrelate" (info unrelate (progDesc "Remove the relationship between two tickets"))
+  , command "query" (info query (progDesc "Search for tickets"))
+  , command "init" (info initialize (progDesc "Initializes an empty ticket system"))
+  , command "tag" (info tag (progDesc "Applies some tags to tickets"))
+  , command "validate" (info validate (progDesc "Validate the ticket system"))
+  ]
+  where
+    mods = header "Ticket Manager!" <> footer "Copyright 2021 (c) Samuel Schlesinger" <> progDesc "Allows the user to manage work tickets."
+    tag = fmap CommandStatement $ CreateTags <$> ticketIDArgument "The ID of the ticket you wish to add tags to" <*> (Set.fromList <$> many tagOption)
+    tagOption = strOption (long "tag" <> short 'x' <> metavar "TAG" <> help "The tag you wish to label the ticket with")
+    ticketIDArgument h = strArgument (metavar "TICKET_ID" <> help h)
+    nameOption = strOption (long "name" <> short 'n' <> metavar "NAME" <> help "The name of the ticket")
+    statusOption = option statusReadM (long "status" <> short 's' <> metavar "STATUS" <> help "The status of the ticket")
+    statusReadM = byExample
+      [ ("todo", ToDo)
+      , ("in-progress", InProgress)
+      , ("in-review", InReview)
+      , ("complete", Complete)
+      , ("wont-fix", WontFix)
+      ] 
+    descriptionOption = strOption (long "description" <> short 'd' <> metavar "DESCRIPTION" <> help "The description of the ticket")
+    queryOrderingReadM = byExample
+      [("name", OrderByName)
+      ,("id", OrderByID)
+      ,("status", OrderByStatus)
+      ]
+    queryOrderingOption = option queryOrderingReadM (long "ordering" <> short 'o' <> metavar "ORDERING" <> help "The desired ordering of the resulting tickets (can use multiple). Can be name, id, or status.")
+    queryLimitOption = option (Limit . Just <$> auto) (long "limit" <> short 'l' <> metavar "LIMIT" <> help "The maximum number of tickets to show" <> value (Limit Nothing))
+    relationshipTypeOption = option relationshipTypeReadM (long "relationship-type" <> short 'r' <> metavar "RELATIONSHIP_TYPE" <> help "The type of the relationship to set. Can be blocks or subsumes.")
+    relationshipTypeReadM = byExample
+      [ ("blocks", Blocks)
+      , ("subsumes", Subsumes)
+      ]
+    filterOption =
+          (FilterByName <$> nameOption)
+      <|> (FilterByTag <$> tagOption)
+      <|> (FilterByID <$> idOption)
+      <|> (FilterByStatus <$> statusOption)
+      <|> (FilterByRelationshipTo Blocks <$> blocksTargetOption)
+      <|> (FilterByRelationshipTo Subsumes <$> subsumesTargetOption)
+      <|> (FilterByRelationshipFrom Blocks <$> blockedByTargetOption)
+      <|> (FilterByRelationshipFrom Subsumes <$> subsumedByTargetOption)
+    ticketOption l s h m = strOption (long l <> short s <> help h <> metavar m)
+    blocksTargetOption = ticketOption "blocks" 'b' "Filters tickets to ones blocking the given ticket" "BLOCKS"
+    subsumesTargetOption = ticketOption "subsumes" 's' "Filters tickets to ones subsuming the given ticket" "SUBSUMES"
+    blockedByTargetOption = ticketOption "blocked-by" 'p' "Filters tickets to ones blocked by the given ticket" "BLOCKED_BY"
+    subsumedByTargetOption = ticketOption "subsumed-by" 'k' "Filters tickets to ones subsumed by the given ticket" "SUBSUMED_BY"
+    idOption = ticketOption "id" 'i' "Filters tickets to a single ID" "TICKET_ID"
+    targetTicketIDOption = ticketOption "target-ticket-id" 't' "The ID of the ticket which is the target of the new relationship" "TARGET"
+    maybeOpt x = (Just <$> x) <|> pure Nothing
+    create = fmap CommandStatement $ CreateTicket <$> ticketIDArgument "An unused ID for the new ticket" <*> (Ticket <$> nameOption <*> descriptionOption <*> statusOption)
+    edit = fmap CommandStatement $ ChangeTicket <$> ticketIDArgument "The ID of the ticket you want to edit" <*> (TicketDiff <$> maybeOpt nameOption <*> maybeOpt descriptionOption <*> maybeOpt statusOption)
+    relate = fmap CommandStatement $ CreateRelationship <$> ticketIDArgument "The ID of the source ticket for the relationship" <*> relationshipTypeOption <*> targetTicketIDOption
+    unrelate = fmap CommandStatement $ RemoveRelationship <$> ticketIDArgument "The ID of the source ticket for the relationship you wish to remove" <*> relationshipTypeOption <*> targetTicketIDOption
+    query = fmap QueryStatement $ Query <$> many filterOption <*> many queryOrderingOption <*> queryLimitOption
+    initialize = pure InitializeStatement
+    validate = pure ValidateStatement
+
+byExample :: [(String, t)] -> ReadM t
+byExample xs = maybeReader (\x -> Just (lookup x xs)) >>= maybe (readerAbort (ErrorMsg $ "Invalid ticket status, perhaps you meant to try one of: " <> intercalate ", " (fmap fst xs))) pure
+
 main :: IO ()
 main = customExecParser ps parser >>= program where
   ps :: ParserPrefs
@@ -19,77 +105,3 @@ main = customExecParser ps parser >>= program where
     , showHelpOnEmpty
     , columns 80
     ]
-  program :: TicketStatement -> IO ()
-  program ticketStatement = lookupEnv "TICKET_SYSTEM" >>= \case
-    Nothing -> do
-      fail "No TICKET_SYSTEM environment variable set"
-    Just filepath -> do
-      case ticketStatement of
-        CommandStatement cmd -> executeCommands filepath [cmd]
-        QueryStatement q -> withTicketSystem filepath (mapM_ print . queryModel q . ticketModel)
-        InitializeStatement -> do
-          doesFileExist filepath >>= \case
-            True -> fail "Trying to initialize a pre-existing ticket system"
-            False -> BS.writeFile filepath (encode emptyTicketSystem)
-        ValidateStatement -> do
-          withTicketSystem filepath \ts -> do
-            case appendCommands (ticketCommands ts) emptyTicketSystem of
-              Nothing -> fail "Ticket system's commands are invalid"
-              Just ts' -> do
-                when (ts' /= ts) (fail "Ticket system's commands do not lead to the model present")
-                putStrLn "Ticket system is valid"
-  parser :: ParserInfo TicketStatement
-  parser = flip info mods . hsubparser . mconcat $
-    [ command "create" (info create (progDesc "Creates a new ticket"))
-    , command "edit" (info edit (progDesc "Edits the name, description, and/or status of an existing ticket"))
-    , command "relate" (info relate (progDesc "Relates two tickets to each other"))
-    , command "unrelate" (info unrelate (progDesc "Removes the relationship between two tickets"))
-    , command "query" (info query (progDesc "Search for tickets"))
-    , command "init" (info initialize (progDesc "Initializes an empty ticket system"))
-    , command "tag" (info tag (progDesc "Applies some tags to tickets"))
-    , command "validate" (info validate (progDesc "Validate the ticket system"))
-    ]
-  mods = header "Ticket Manager!" <> footer "Copyright 2021 (c) Samuel Schlesinger" <> progDesc "Allows the user to manage work tickets."
-  tag = fmap CommandStatement $ CreateTags <$> ticketIDArgument <*> (Set.fromList <$> many tagOption)
-  tagOption = strOption (long "tag" <> short 'x')
-  ticketIDArgument = strArgument (metavar "TICKET_ID")
-  nameOption = strOption (long "name" <> short 'n')
-  statusOption = option statusReadM (long "status" <> short 's')
-  statusReadM = byExample
-    [ ("todo", ToDo)
-    , ("in-progress", InProgress)
-    , ("in-review", InReview)
-    , ("complete", Complete)
-    , ("wont-fix", WontFix)
-    ] 
-  descriptionOption = strOption (long "description" <> short 'd')
-  queryOrderingReadM = byExample
-    [("name", OrderByName)
-    ,("id", OrderByID)
-    ,("status", OrderByStatus)
-    ]
-  queryOrderingOption = option queryOrderingReadM (long "ordering" <> short 'o')
-  queryLimitOption = option (Limit . Just <$> auto) (long "limit" <> short 'l' <> value (Limit Nothing))
-  relationshipTypeOption = option relationshipTypeReadM (long "relationship-type" <> short 'r')
-  relationshipTypeReadM = byExample
-    [ ("blocks", Blocks)
-    , ("subsumes", Subsumes)
-    ]
-  filterOption = (FilterByName <$> nameOption) <|> (FilterByTag <$> tagOption) <|> (FilterByID <$> idOption) <|> (FilterByStatus <$> statusOption) <|> (FilterByRelationshipTo Blocks <$> blocksTargetOption) <|> (FilterByRelationshipTo Subsumes <$> subsumesTargetOption) <|> (FilterByRelationshipFrom Blocks <$> blockedByTargetOption) <|> (FilterByRelationshipFrom Subsumes <$> subsumedByTargetOption)
-  blocksTargetOption = TicketID <$> strOption (long "blocks" <> short 'b')
-  subsumesTargetOption = TicketID <$> strOption (long "subsumes" <> short 's')
-  blockedByTargetOption = TicketID <$> strOption (long "blocked-by" <> short 'p')
-  subsumedByTargetOption = TicketID <$> strOption (long "subsumed-by" <> short 'k')
-  idOption = TicketID <$> strOption (long "id" <> short 'i')
-  targetTicketIDOption = strOption (long "target-ticket-id" <> short 't')
-  maybeOpt x = (Just <$> x) <|> pure Nothing
-  create = fmap CommandStatement $ CreateTicket <$> ticketIDArgument <*> (Ticket <$> nameOption <*> descriptionOption <*> statusOption)
-  edit = fmap CommandStatement $ ChangeTicket <$> ticketIDArgument <*> (TicketDiff <$> maybeOpt nameOption <*> maybeOpt descriptionOption <*> maybeOpt statusOption)
-  relate = fmap CommandStatement $ CreateRelationship <$> ticketIDArgument <*> relationshipTypeOption <*> targetTicketIDOption
-  unrelate = fmap CommandStatement $ RemoveRelationship <$> ticketIDArgument <*> relationshipTypeOption <*> targetTicketIDOption
-  query = fmap QueryStatement $ Query <$> many filterOption <*> many queryOrderingOption <*> queryLimitOption
-  initialize = pure InitializeStatement
-  validate = pure ValidateStatement
-
-byExample :: [(String, t)] -> ReadM t
-byExample xs = maybeReader (\x -> Just (lookup x xs)) >>= maybe (readerAbort (ErrorMsg $ "Invalid ticket status, perhaps you meant to try one of: " <> intercalate ", " (fmap fst xs))) pure
--- test/Test.hs
+++ test/Test.hs
@@ -4,6 +4,8 @@ module Main where
 
 import Data.Ticket
 import Test.QuickCheck
+import System.Exit (exitFailure)
+import System.IO (hPutStrLn, stderr)
 
 args :: Args
 args = stdArgs
@@ -12,9 +14,40 @@ args = stdArgs
   , maxSize = 10000
   }
 
+require :: Bool -> String -> IO ()
+require t msg = if t then pure () else hPutStrLn stderr msg >> exitFailure
+
+commandsInvalid :: [Command] -> Bool
+commandsInvalid = testCommands (maybe True (const False))
+
+commandsValid :: [Command] -> Bool
+commandsValid = testCommands (maybe False (const True))
+
+testCommands :: (Maybe TicketSystem -> Bool) -> [Command] -> Bool
+testCommands f cs = f $ appendCommands cs emptyTicketSystem
+
 main :: IO ()
 main = do
-  quickCheckWith args \(unValidCommandSequence -> commands) ->
+  -- Make sure the valid command sequence generator generates valid command sequences
+  check \(unValidCommandSequence -> commands) ->
     property $ maybe False (const True) $ appendCommands commands emptyTicketSystem
-  quickCheckWith args \system ->
+  -- Make sure the system generator produces valid systems
+  check \system ->
     property $ appendCommands (ticketCommands system) emptyTicketSystem == Just system
+  -- Make sure we can't create tickets with duplicate ids
+  check \ticketID1 ticket1 ticket2 ->
+    property $ commandsInvalid [CreateTicket ticketID1 ticket1, CreateTicket ticketID1 ticket2]
+  -- Check edit command
+  check \ticketID1 ticket1 ticketDiff ->
+    property $ commandsValid [CreateTicket ticketID1 ticket1, ChangeTicket ticketID1 ticketDiff]
+  -- Check relationship commands
+  check \ticketID1 ticket1 ticket2 rel -> property do
+    ticketID2 <- suchThat arbitrary (/= ticketID1)
+    pure $ commandsValid
+      [ CreateTicket ticketID1 ticket1, CreateTicket ticketID2 ticket2
+      , CreateRelationship ticketID1 rel ticketID2
+      , RemoveRelationship ticketID1 rel ticketID2
+      ]
+  where
+    check :: Testable prop => prop -> IO ()
+    check = quickCheckWith args
